c5a3dd1c0821596eeff5593eb7fb214e28cc52a1
Saad Hussain <saad_hussain94@hotmail.com>
2016-01-01 20:28:31 +0500

    005. done findVerbPairs()

--- test_pos.java
+++ test_pos.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 import edu.stanford.nlp.ling.Sentence;
@@ -20,38 +21,70 @@ import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.process.TokenizerFactory;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
-class test_pos {
+//=================================================================================
+//=================================================================================
+
+class Word_Pair implements Comparable<Word_Pair> {
+  public int    count;
+  public String word_one;
+  public String word_two;
+
+  public Word_Pair(String w1, String w2) {
+    w1 = w1.toLowerCase();
+    w2 = w2.toLowerCase();
+    if (w1.compareTo(w2) < 0) {
+      word_one = w1;
+      word_two = w2;
+    } else {
+      word_one = w2;
+      word_two = w1;
+    }
+
+    count = 1;
+  }
+
+  public String toString() {
+    return word_one + " - " + word_two;
+  }
+
+  public String print() {
+    return Integer.toString(count) + "\t" + toString();
+  }
+
+  public boolean equals(Word_Pair wp) {
+    return word_one.equals(wp.word_one) && word_two.equals(wp.word_two);
+  }
+
+  public void increment() {
+    count++;
+  }
+
+  @Override
+  public int compareTo(Word_Pair another) {
+    if (this.toString().compareTo(another.toString()) < 0) {
+        return -1;
+    } else {
+        return 1;
+    }
+  }
+}
+
+//=================================================================================
+//=================================================================================
+
+public class test_pos {
   // static List<String>   phrases_causal            = Arrays.asList("because", "for this reason", "for that reason", "consequently", "as a consequence of", "as a result of");
   // static List<String>   phrases_non_causal        = Arrays.asList("but", "in short", "in other words", "whereas", "on the other hand", "nevertheless", "nonetheless", "in spite of", "in contrast", "however", "even", "though", "despite the fact", "conversely", "although"); 
   // static List<Integer>  count_phrases_causal      = Arrays.asList(0, 0, 0, 0, 0, 0);
   // static List<Integer>  count_phrases_non_causal  = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
   static List<File>     all_files         = new ArrayList<>();
+  static List<Word_Pair>all_verb_pairs    = new ArrayList<>();
   static List<String>   phrases_all       = Arrays.asList("because", "for this reason", "for that reason", "consequently", "as a consequence of", "as a result of", "but", "in short", "in other words", "whereas", "on the other hand", "nevertheless", "nonetheless", "in spite of", "in contrast", "however", "even", "though", "despite the fact", "conversely", "although");
   static List<Integer>  count_phrases_all = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
   static List<Integer>  length_phrases_all= Arrays.asList(1, 3, 3, 1, 4, 4, 1, 2, 3, 1, 4, 1, 1, 3, 2, 1, 1, 1, 3, 1, 1);
   static String         dirName           = System.getProperty("user.dir") + "\\textfiles\\test";
   static String         modelFile         = "models\\english-left3words-distsim.tagger";
   static PrintWriter    pw;
-
-  //=================================================================================
-  //=================================================================================
-
-  public class Word_Pair {
-    public String word_one;
-    public String word_two;
-
-    public Word_Pair(String w1, String w2) {
-      w1 = w1.toLowerCase();
-      w2 = w2.toLowerCase();
-      if (w1.compareTo(w2) < 0) {
-        word_one = w1;
-        word_two = w2;
-      } else {
-        word_one = w2;
-        word_two = w1;
-      }
-    }
-  }
 
   //=================================================================================
   //=================================================================================
@@ -76,24 +109,75 @@ class test_pos {
   //=================================================================================
 
   public static void findVerbPairs(List<Integer> toCheck, List<TaggedWord> tSentence) {
+    boolean printer = true;
+
     // Go through each (non-)causal phrase to be checked in the current sentence.
     for (Integer id : toCheck) {
       List<String> phrase = Arrays.asList(phrases_all.get(id).split(" "));
 
       // Go through each tagged word in the sentence.
       for (int i = 0; i < tSentence.size(); i++) {
-        boolean found = false;
 
+        // Find the position of the (non-)causal phrase.
+        boolean found = false;
+        if (tSentence.get(i).word().toLowerCase().equals(phrase.get(0))) {
+          found = true;
+          if (phrase.size() > 1) {
+            for (int j = 1; j < phrase.size(); j++) {
+              if (tSentence.size() <= i+j || !tSentence.get(i+j).word().toLowerCase().equals(phrase.get(j))) {
+                found = false;
+                break;
+              }
+            }
+          }
+
+          if (found) {
+            // pw.println("FOUND");
+            // for (String s : phrase) {
+            //   pw.print(s + " ");
+            // }            
+            // pw.println("\n");
+
+            List<String> verbsBefore = new ArrayList<>();
+            List<String> verbsAfter  = new ArrayList<>();
+
+            // Check for verbs occurring before the unambiguous discourse marker.
+            for (int j = 0; j < i; j++) {
+              if (tSentence.get(j).tag().startsWith("VB")) {
+                verbsBefore.add(tSentence.get(j).word());
+              }
+            }
+
+            // Check for verbs occurring after the unambiguous discourse marker.
+            for (int j = i+length_phrases_all.get(id); j < tSentence.size(); j++) {
+              if (tSentence.get(j).tag().startsWith("VB")) {
+                verbsAfter.add(tSentence.get(j).word());
+              }
+            }
+
+            // If verbs exist both before and after the discourse marker, form all possible pairs of them.
+            for (String s1 : verbsBefore) {
+              for (String s2 : verbsAfter) {
+
+                // if the verbs are different
+                if (!s1.toLowerCase().equals(s2.toLowerCase())) {
+                  all_verb_pairs.add(new Word_Pair(s1, s2));
+                }
+              }
+            }
+          }
+        }
 
         // Check if the tagged word is a verb of any kind (there are 6 kinds of verbs; HOW_TO_RUN.txt).
-        if (tWord.tag().startsWith("VB")) {
-          if (tWord.tag().equals("VB")) {
-            pw.println("\tTAG: " + tWord.tag() + " \tWORD: " + tWord.word());  
+        if (printer && tSentence.get(i).tag().startsWith("VB")) {
+          if (tSentence.get(i).tag().equals("VB")) {
+            pw.println("\tTAG: " + tSentence.get(i).tag() + " \tWORD: " + tSentence.get(i).word());  
           } else {
-            pw.println("\tTAG: " + tWord.tag() + "\tWORD: " + tWord.word());  
+            pw.println("\tTAG: " + tSentence.get(i).tag() + "\tWORD: " + tSentence.get(i).word());  
           }
         }
       }
+      printer = false;
     }
   }
 
@@ -137,7 +221,7 @@ class test_pos {
       String fileName = all_files.get(id).getPath();
       pw.print("\n***\n" + fileName + "\n***\n");
 
-      // Check for occurrences for the (non-)causal strings in the current document, incremement the occurrence counter for use in IDF function.
+      // Check for occurrences for the (non-)causal strings in the current document, increment the occurrence counter for use in IDF function.
       // Find out which (non-)causal string to check for in the current document
       List<Integer> toCheck = IDF_Counter(id);
 
@@ -176,6 +260,23 @@ class test_pos {
       pw.print(count_phrases_all.get(i) + ", ");
     }
     pw.print("\n\n");
+
+    // Removing duplicate Verb Pairs and incrementing their count.
+    Collections.sort(all_verb_pairs);
+    int size = all_verb_pairs.size() - 1;
+    for (int i = 0; i < size; i++) {
+      if (all_verb_pairs.get(i).equals(all_verb_pairs.get(i+1))) {
+        all_verb_pairs.get(i).increment();
+        all_verb_pairs.remove(i+1);
+        i--;
+        size--;
+      }
+    }
+
+    // Printing the Verb Pairs.
+    for (Word_Pair wp : all_verb_pairs) {
+      pw.println(wp.print());
+    }
 
     pw.close();
   }
