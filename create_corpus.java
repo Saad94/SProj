34885d70c1107b506eb1ec80535b2973e3fdbfef
Saad Hussain <saad_hussain94@hotmail.com>
2016-02-05 19:20:51 +0500

    010. separated create_corpus and analyze_files into separate components. finished implementation of f_I(). needs to be tested

--- create_corpus.java
+++ create_corpus.java
@@ -50,7 +50,7 @@ class Word_Count implements Comparable<Word_Count> {
   }
 
   public String print() {
-    return Integer.toString(documentCount) + "            " + Integer.toString(actualCount) + "          " + word;
+    return Integer.toString(documentCount) + " " + Integer.toString(actualCount) + " " + word;
   }
 
   public boolean equals(Word_Count wc) {
@@ -119,7 +119,7 @@ class Word_Pair implements Comparable<Word_Pair> {
   }
 
   public String print() {
-    return Integer.toString(documentCount) + "\t" + Integer.toString(actualCount) + "\t" + toString();
+    return Integer.toString(documentCount) + " " + Integer.toString(actualCount) + " " + toString();
   }
 
   @Override
@@ -164,7 +164,7 @@ class Word_Pair implements Comparable<Word_Pair> {
 //=================================================================================
 //=================================================================================
 
-public class test_pos {
+public class create_corpus {
   static List<File>       all_files         = new ArrayList<>();
   static List<Word_Pair>  all_verb_pairs    = new ArrayList<>();
   static List<Word_Count> doc_count_words   = new ArrayList<>();
@@ -178,27 +178,8 @@ public class test_pos {
   //=================================================================================
   //=================================================================================
 
-  public static Word_Count find_WC(String word) {
-    for (Word_Count wc : doc_count_words) {
-      if (wc.word.equals(word)) {
-        return wc;
-      }
-    }
-    return null;
-  }
-
-  //=================================================================================
-  //=================================================================================
-
-  public static List<Integer> docWordCounter(int id) throws Exception {
-    List<Integer> returnValue = new ArrayList<>();
+  public static void docWordCounter(int id) throws Exception {
     String content = new Scanner(all_files.get(id)).useDelimiter("\\Z").next().toLowerCase();
-    for (int i = 0; i < phrases_all.size(); i++) {
-      if (content.contains(phrases_all.get(i))) {
-        returnValue.add(i);
-      }
-    }
-
     content = content.replace(".", "");
     content = content.replace(",", "");
 
@@ -226,74 +207,6 @@ public class test_pos {
       }
       wc.actualIncrement(i);
     }
-    
-    return returnValue;
-  }
-
-  //=================================================================================
-  //=================================================================================
-
-  public static double IDF(Word_Pair wp) {
-    double one = idf(wp.word_one);
-    double two = idf(wp.word_two);
-    double three = idf(wp);
-
-    pw.print(wp.print() + "\n\t");
-    pw.println(Double.toString(one) + " * " + Double.toString(two) + " * " + Double.toString(three));
-    return one * two * three;
-  }
-
-  public static double idf(String word) {
-    double ans = 1.0 + find_WC(word).documentCount;
-    return all_files.size() / ans;
-  }
-
-  public static double idf(Word_Pair wp) {
-    double ans = 1.0 + wp.documentCount;
-    return all_files.size() / ans;
-  }
-
-  //=================================================================================
-  //=================================================================================
-
-  public static double P(String word) {
-    return 0.0;
-  }
-
-  public static double P(Word_Pair wp) {
-    return 0.0;
-  }
-
-  public static double PMI(Word_Pair wp) {
-    return Math.log(P(wp) / (P(wp.word_one) * P(wp.word_two)));
-  }
-
-  //=================================================================================
-  //=================================================================================
-
-  public static double CD(Word_Pair wp) {
-    return PMI(wp) * max(wp) * IDF(wp);
-  }
-
-  //=================================================================================
-  //=================================================================================
-
-  public static double max(Word_Pair wp) {
-    return 0.0;
-  }
-
-  //=================================================================================
-  //=================================================================================
-
-  public static double PS_I(Word_Pair wp) {
-    return 0.0;
-  }
-
-  //=================================================================================
-  //=================================================================================
-
-  public static Word_Pair f_I() {
-    return null;
   }
 
   //=================================================================================
@@ -331,124 +244,74 @@ public class test_pos {
     return phraseLocations;
   }
 
-  public static void findVerbPairs(List<Integer> toCheck, List<TaggedWord> tSentence) {
-    boolean printer = true;
+  public static void findVerbPairs(List<TaggedWord> tSentence) {
+    List<Pair> phraseLocations = findPhraseLocations(tSentence);
+    int start = 0;
+    int end = 0;
 
-    // // Go through each (non-)causal phrase to be checked in the current sentence.
-    // for (Integer id : toCheck) {
-    //   List<String> phrase = Arrays.asList(phrases_all.get(id).split(" "));
+    for (int i = 0; i < phraseLocations.size(); i++) {
+      if (i == phraseLocations.size() - 1) {
+        end = tSentence.size();
+      } else {
+        end = phraseLocations.get(i+1).x;
+      }
 
-    //   // Go through each tagged word in the sentence.
-    //   for (int i = 0; i < tSentence.size(); i++) {
+      List<String> verbsBefore = new ArrayList<>();
+      List<String> verbsAfter  = new ArrayList<>();
 
-    //     // Find the position of the (non-)causal phrase.
-    //     boolean found = false;
-    //     if (tSentence.get(i).word().toLowerCase().equals(phrase.get(0))) {
-    //       found = true;
-    //       if (phrase.size() > 1) {
-    //         for (int j = 1; j < phrase.size(); j++) {
-    //           if (tSentence.size() <= i+j || !tSentence.get(i+j).word().toLowerCase().equals(phrase.get(j))) {
-    //             found = false;
-    //             break;
-    //           }
-    //         }
-    //       }
+      // Check for verbs occurring before the unambiguous discourse marker.
+      for (int j = start; j < phraseLocations.get(i).x; j++) {
+        if (tSentence.get(j).tag().startsWith("VB")) {
+          verbsBefore.add(tSentence.get(j).word());
 
-    //       if (found) {
-
-        List<Pair> phraseLocations = findPhraseLocations(tSentence);
-        int start = 0;
-        int end = 0;
-
-        for (int i = 0; i < phraseLocations.size(); i++) {
-          if (i == phraseLocations.size() - 1) {
-            end = tSentence.size();
+          // Printing
+          if (tSentence.get(j).tag().equals("VB")) {
+            pw.println("\tTAG_BEFORE: " + tSentence.get(j).tag() + " \tWORD: " + tSentence.get(j).word());  
           } else {
-            end = phraseLocations.get(i+1).x;
-          }
-
-          List<String> verbsBefore = new ArrayList<>();
-          List<String> verbsAfter  = new ArrayList<>();
-
-          // Check for verbs occurring before the unambiguous discourse marker.
-          for (int j = start; j < phraseLocations.get(i).x; j++) {
-            if (tSentence.get(j).tag().startsWith("VB")) {
-              verbsBefore.add(tSentence.get(j).word());
-
-              // Printing
-              if (tSentence.get(j).tag().equals("VB")) {
-                pw.println("\tTAG_BEFORE: " + tSentence.get(j).tag() + " \tWORD: " + tSentence.get(j).word());  
-              } else {
-                pw.println("\tTAG_BEFORE: " + tSentence.get(j).tag() + "\tWORD: " + tSentence.get(j).word());  
-              }
-            }
-          }
-
-          // Check for verbs occurring after the unambiguous discourse marker.
-          for (int j = phraseLocations.get(i).x+phraseLocations.get(i).y; j < end; j++) {
-            if (tSentence.get(j).tag().startsWith("VB")) {
-              verbsAfter.add(tSentence.get(j).word());
-              
-              // Printing
-              if (tSentence.get(j).tag().equals("VB")) {
-                pw.println("\tTAG_AFTER : " + tSentence.get(j).tag() + " \tWORD: " + tSentence.get(j).word());  
-              } else {
-                pw.println("\tTAG_AFTER : " + tSentence.get(j).tag() + "\tWORD: " + tSentence.get(j).word());  
-              }
-            }
-          }
-
-          start = phraseLocations.get(i).x + phraseLocations.get(i).y;
-
-          // pw.println("BEFORE");
-          // for (Word_Pair wp : all_verb_pairs) {
-          //   pw.println(wp.print());
-          // }
-          // pw.println("\n\n");
-
-          // If verbs exist both before and after the discourse marker, form all possible pairs of them.
-          for (String s1 : verbsBefore) {
-            for (String s2 : verbsAfter) {
-
-              // if the verbs are different
-              if (!s1.toLowerCase().equals(s2.toLowerCase())) {
-                all_verb_pairs.add(new Word_Pair(s1, s2));
-              }
-            }
-          }
-
-          // pw.println("AFTER");
-          // for (Word_Pair wp : all_verb_pairs) {
-          //   pw.println(wp.print());
-          // }
-          // pw.println("\n\n");
-        }
-
-        // Removing duplicate Verb Pairs and incrementing their count.
-        Collections.sort(all_verb_pairs);
-        int size = all_verb_pairs.size() - 1;
-        for (int j = 0; j < size; j++) {
-          if (all_verb_pairs.get(j).equals(all_verb_pairs.get(j+1))) {
-            all_verb_pairs.get(j).actualIncrement();
-            all_verb_pairs.remove(j+1);
-            j--;
-            size--;
+            pw.println("\tTAG_BEFORE: " + tSentence.get(j).tag() + "\tWORD: " + tSentence.get(j).word());  
           }
         }
-        //   }
-        // }
+      }
 
-    //     // Check if the tagged word is a verb of any kind (there are 6 kinds of verbs; HOW_TO_RUN.txt).
-    //     if (printer && tSentence.get(i).tag().startsWith("VB")) {
-          // if (tSentence.get(i).tag().equals("VB")) {
-          //   pw.println("\tTAG: " + tSentence.get(i).tag() + " \tWORD: " + tSentence.get(i).word());  
-          // } else {
-          //   pw.println("\tTAG: " + tSentence.get(i).tag() + "\tWORD: " + tSentence.get(i).word());  
-          // }
-    //     }
-    //   }
-    //   printer = false;
-    // }
+      // Check for verbs occurring after the unambiguous discourse marker.
+      for (int j = phraseLocations.get(i).x+phraseLocations.get(i).y; j < end; j++) {
+        if (tSentence.get(j).tag().startsWith("VB")) {
+          verbsAfter.add(tSentence.get(j).word());
+          
+          // Printing
+          if (tSentence.get(j).tag().equals("VB")) {
+            pw.println("\tTAG_AFTER : " + tSentence.get(j).tag() + " \tWORD: " + tSentence.get(j).word());  
+          } else {
+            pw.println("\tTAG_AFTER : " + tSentence.get(j).tag() + "\tWORD: " + tSentence.get(j).word());  
+          }
+        }
+      }
+
+      start = phraseLocations.get(i).x + phraseLocations.get(i).y;
+
+      // If verbs exist both before and after the discourse marker, form all possible pairs of them.
+      for (String s1 : verbsBefore) {
+        for (String s2 : verbsAfter) {
+
+          // if the verbs are different
+          if (!s1.toLowerCase().equals(s2.toLowerCase())) {
+            all_verb_pairs.add(new Word_Pair(s1, s2));
+          }
+        }
+      }
+    }
+
+    // Removing duplicate Verb Pairs and incrementing their count.
+    Collections.sort(all_verb_pairs);
+    int size = all_verb_pairs.size() - 1;
+    for (int j = 0; j < size; j++) {
+      if (all_verb_pairs.get(j).equals(all_verb_pairs.get(j+1))) {
+        all_verb_pairs.get(j).actualIncrement();
+        all_verb_pairs.remove(j+1);
+        j--;
+        size--;
+      }
+    }
   }
 
   //=================================================================================
@@ -493,7 +356,7 @@ public class test_pos {
 
       // Check for occurrences for the (non-)causal strings in the current document, increment the occurrence counter for use in IDF function.
       // Find out which (non-)causal string to check for in the current document
-      List<Integer> toCheck = docWordCounter(id);
+      docWordCounter(id);
 
       // Open the file.
       BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "utf-8"));
@@ -526,7 +389,7 @@ public class test_pos {
         }
 
         // Find pairs of verbs before and after the unambiguous discourse markers.
-        findVerbPairs(toCheck, tSentence);
+        findVerbPairs(tSentence);
 
         // Incrementing the document count for the pairs.
         for (int i = 0; i < oldList.size(); i++) {
@@ -559,8 +422,25 @@ public class test_pos {
 
     // Printing a sample IDF.
     // pw.print("\nIDF\n\t");
-    // IDF(all_verb_pairs.get(14));
+    // IDF(all_verb_pairs.get(13));
 
+    // Testing a max function call.
+    // pw.println("MAX = " + Double.toString(max(all_verb_pairs.get(3))));
+
+    pw.close();
+
+    // Output the Words to a file called dictionary.txt;
+    pw = new PrintWriter(new File("dictionary.txt"));
+    for (int i = 0; i < doc_count_words.size(); i++) {
+      pw.println(doc_count_words.get(i).print());
+    }
+    pw.close();
+
+    // Output the Verb-Verb pairs to a file called verb-verb.txt;
+    pw = new PrintWriter(new File("verb-verb.txt"));
+    for (Word_Pair wp : all_verb_pairs) {
+      pw.println(wp.print());
+    }
     pw.close();
   }
 }
